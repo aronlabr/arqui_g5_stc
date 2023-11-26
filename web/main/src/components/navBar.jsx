444f5275468016efa154aafa75046217227c3bad
Aaron Blas <luisaaron.blas@unmsm.edu.pe>
2023-11-25 22:55:34 -0500

    Add layout component and update navbar links

--- web/main/src/components/navBar.jsx
+++ web/main/src/components/navBar.jsx
@@ -11,16 +11,22 @@ export default function NavBar() {
         <Navbar.Collapse id="navbar" className="justify-content-lg-between">
           <Nav className="me-auto my-2 my-lg-0" navbarScroll>
             <Nav.Item className="pe-lg-2">
-              <Link href={'/dashboard'}>Dashboard</Link>
+              <Link href={'/admin/'}>Dashboard</Link>
             </Nav.Item>
             <Nav.Item className="pe-lg-2">
-              <Link href={'/incidencias/1'}>Incidencia de Prueba</Link>
+              <Link href={'/admin/incidentes'}>Incidencias</Link>
             </Nav.Item>
             <Nav.Item className="pe-lg-2">
-              <Link href={'/notificaciones'}>Notificaciones</Link>
+              <Link href={'/admin/incidentes/1'}>Incidencia de Prueba</Link>
             </Nav.Item>
             <Nav.Item className="pe-lg-2">
-              <Link href={'/tecnicos'}>Tecnicos</Link>
+              <Link href={'/admin/visitas'}>Visitas</Link>
+            </Nav.Item>
+            <Nav.Item className="pe-lg-2">
+              <Link href={'/admin/notific'}>Notificaciones</Link>
+            </Nav.Item>
+            <Nav.Item className="pe-lg-2">
+              <Link href={'/admin/tecnicos'}>Tecnicos</Link>
             </Nav.Item>
           </Nav>
           <BtnLogout />
